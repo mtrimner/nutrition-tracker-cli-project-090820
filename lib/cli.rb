9105abcd5842efdd5f8eb6b5713c608a1991d147
mtrimner <matt.trimner@gmail.com>
2020-09-10 13:27:31 -0700

    Refactored code to reduce number of lines. Added a #round() method to the output of the #dispalys_food_info() method to reduce the amount of decimal places the macronutrients will return.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,26 +1,24 @@
-require_relative '../lib/branded_food'
-require_relative '../lib/common_food'
-require_relative '../lib/cli'
-require 'uri'
-# require 'httpclient'
-require 'json'
-require 'pry'
-require 'net/http'
+# require_relative '../lib/branded_food'
+# require_relative '../lib/common_food'
+# require_relative '../lib/cli'
+# require 'uri'
+# require 'json'
+# require 'pry'
+# require 'net/http'
 
 class CLI 
  
     def run
-        puts "Hello! Please enter a food below."
+        puts "Welcome to the Nutrition Database Lookup!"
+        puts "Hello! Please TYPE a food below."
         input = gets.chomp
-        binding.pry
         API.food_search(input)
-        binding.pry
-        puts "Please select an option below!"
         list_foods
         menu
-        # binding.pry
     end
+    
     def list_foods
+        puts "Please select number below!"
         puts "Common Suggestions:"
         CommonFood.all.each.with_index(1) do |food, i|
             puts "#{i}. #{food.food_name}"
@@ -29,33 +27,26 @@ class CLI
         BrandedFood.all.each.with_index(CommonFood.all.length+1) do |food, i|
             puts "#{i}. #{food.brand_name} - #{food.food_name}"
         end
-
+        puts "Please select a number above!"
     end
     
 
 
     def menu
-        puts "Please select a number to get food info."
             input = gets.chomp
         if input.to_i.between?(1, CommonFood.all.length)
             food_choice = CommonFood.all[input.to_i-1]
-            # binding.pry
             selected_food_name = food_choice.food_name
-            # binding.pry
             selected_food_info = API.get_common_food_nutrients(selected_food_name)
-            # binding.pry
             displays_food_info(selected_food_info)
-            binding.pry
         elsif input.to_i.between?(CommonFood.all.length+1, BrandedFood.all.length+CommonFood.all.length)
             food_choice = BrandedFood.all[input.to_i-1 - CommonFood.all.length]
-            # binding.pry
             selected_food_id = food_choice.nix_item_id
-            # binding.pry
             selected_food_info = API.get_branded_food_nutrients(selected_food_id)
-            # binding.pry
             displays_food_info(selected_food_info)
         else 
             puts "Number not valid, please try again."
+            menu
         end
         puts "Would you like to lookup more food?"
         puts "Please enter Y or N"
@@ -63,10 +54,10 @@ class CLI
         if another_lookup == "y"
             BrandedFood.reset
             CommonFood.reset
-            puts "Please search for another food below."
+            puts "Please search for another food!"
             input = gets.chomp
             API.food_search(input)
-            puts "Please select an option below!"
+            # puts "Please select an option below!"
             list_foods
             menu
         else another_lookup == "no"
@@ -77,12 +68,12 @@ class CLI
 
     end
 
-    def displays_food_info(selected_food_info)
-        puts "#{selected_food_info[:food_name]} has the following per #{selected_food_info[:serving_qty]} #{selected_food_info[:serving_unit]}"
-        puts "Total calories = #{selected_food_info[:nf_calories]}"
-        puts "Protein = #{selected_food_info[:nf_protein]}g"
-        puts "Total Fat = #{selected_food_info[:nf_total_fat]}g"
-        puts "Carbs = #{selected_food_info[:nf_total_carbohydrate]}g"
+    def displays_food_info(food)
+        puts "#{food[:food_name]} has the following per #{food[:serving_qty]} #{food[:serving_unit]}"
+        puts "Total calories = #{food[:nf_calories].round(1)}"
+        puts "Protein = #{food[:nf_protein].round(1)}g"
+        puts "Total Fat = #{food[:nf_total_fat].round(1)}g"
+        puts "Carbs = #{food[:nf_total_carbohydrate].round(1)}g"
     end
 
 end
