7cf99eb0c460fc2e973abbe7dcf36605f49aaa03
mtrimner <matt.trimner@gmail.com>
2020-09-10 14:17:37 -0700

    Deleted the require and require-relative from the top of this file.

--- lib/api.rb
+++ lib/api.rb
@@ -1,20 +1,11 @@
-# require_relative '../lib/branded_food'
-# require_relative '../lib/common_food'
-# require_relative '../lib/cli'
-# require 'uri'
-# require 'json'
-# require 'pry'
-# require 'net/http'
-
 # X-APP-ID = 288d43cd
 # X-APP-KEY = 8fec0adf08110aaf257cb4d4665594e6
 
-#AUTOFILL (Natural Language)
-class API
 
+class API
+    #This will allow users to search in natural language and provide and "autofilled" list of options.
     def self.food_search(food)
         url = URI("https://trackapi.nutritionix.com/v2/search/instant?query=#{food}")
-
         https = Net::HTTP.new(url.host, url.port);
         https.use_ssl = true
         request = Net::HTTP::Get.new(url)
@@ -35,8 +26,8 @@ class API
     end
 
 
-#SEACH ITEM(Branded)
 
+    #This method will allow users to access a database of brandnamed foods and restaurant menus.""
     def self.get_branded_food_nutrients(food_id)
         url = URI("https://trackapi.nutritionix.com/v2/search/item?nix_item_id=#{food_id}")
         https = Net::HTTP.new(url.host, url.port);
@@ -46,15 +37,12 @@ class API
         request["x-app-key"] = "8fec0adf08110aaf257cb4d4665594e6"
         response = https.request(request)
         response_hash = JSON.parse(response.read_body, symbolize_names:true)[:foods][0]
-       
-        # nutrient_hash = response_hash[:foods][0]
-
     end
 
 
 
-#COMMON FOODS
 
+    # This will allow users access to a large USDA database of common foods.
     def self.get_common_food_nutrients(food)
         url = URI("https://trackapi.nutritionix.com/v2/natural/nutrients")
         https = Net::HTTP.new(url.host, url.port);
@@ -66,10 +54,7 @@ class API
         request.body = "query=#{food}"      
         response = https.request(request)
         response_hash = JSON.parse(response.read_body, symbolize_names:true)[:foods][0]
-        # nutrient_hash = response_hash[:foods][0]
-
     end
-
 end
 
 
