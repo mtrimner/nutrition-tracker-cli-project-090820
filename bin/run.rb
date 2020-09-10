2c171f986085bc3f99657d2e5e9b803e09766802
mtrimner <matt.trimner@gmail.com>
2020-09-10 11:55:54 -0700

    Added the required files to the bin/run.rb file. Added the .run command to the run file.

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,13 @@
 #!/usr/bin/env ruby
+
+require_relative '../lib/branded_food'
+require_relative '../lib/common_food'
+require_relative '../lib/cli'
+require_relative '../lib/api'
+require 'uri'
+# require 'httpclient'
+require 'json'
+require 'pry'
+require 'net/http'
+
+CLI.new.run
